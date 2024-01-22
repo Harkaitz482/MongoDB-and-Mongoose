77308a2bc2eaec68b4af3af347b2d14e6ecee36a
Harkaitz482 <harkatrujillo@hotmail.com>
2024-01-22 20:37:29 +0000

    5

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 let Person;
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
