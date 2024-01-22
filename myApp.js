f278dab25130b97d3c5a9f8a21b00385b708b1c4
Harkaitz482 <harkatrujillo@hotmail.com>
2024-01-22 21:59:40 +0000

    3.4

--- myApp.js
+++ myApp.js
@@ -22,18 +22,14 @@ const Person = mongoose.model('Person', personSchema);
 
 
 
-var createAndSavePerson = function(done) {
+const createAndSavePerson = (done) => {
   var janeFonda = new Person({name: "Jane Fonda", age: 84, favoriteFoods: ["eggs", "fish", "fresh fruit"]});
 
   janeFonda.save(function(err, data) {
     if (err) return console.error(err);
-    done(null, data)
-  });
+    done(null, data);
+  }); // Corrected the closing of save function and the function itself
 };
-
-
-
-
 
 
 const createManyPeople = (arrayOfPeople, done) => {
