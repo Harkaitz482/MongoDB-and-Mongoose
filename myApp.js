e8d10ec72988e87a3df77a80a98a52acc535bd13
Harkaitz482 <harkatrujillo@hotmail.com>
2024-01-26 18:02:16 +0000

    3.9

--- myApp.js
+++ myApp.js
@@ -8,6 +8,7 @@ const Schema = mongoose.Schema;
 
 mongoose.connect((process.env.MONGO_URI), { useNewUrlParser: true, useUnifiedTopology: true });
 
+let Person = require('./models/personSchema');
 
 
 const createAndSavePerson = (done) => {
